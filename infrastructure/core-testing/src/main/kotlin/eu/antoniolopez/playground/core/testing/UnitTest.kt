6a3be3bbbed6e0b6bb57c8484f7ca979fdd49de5
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-07-18 23:40:34 +0200

    Fixed livedata error in UnitTests

--- infrastructure/core-testing/src/main/kotlin/eu/antoniolopez/playground/core/testing/UnitTest.kt
+++ infrastructure/core-testing/src/main/kotlin/eu/antoniolopez/playground/core/testing/UnitTest.kt
@@ -4,9 +4,11 @@ import io.mockk.clearAllMocks
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.extension.ExtendWith
 
 @Suppress("IllegalIdentifier")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(InstantExecutorExtension::class)
 abstract class UnitTest {
 
     @BeforeEach
