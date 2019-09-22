84a3acec7955d2f795c07c2a0a955be786ea88e7
antonio lopez <antonio.lopez@metropolis-lab.io>
2019-09-22 19:49:03 +0200

    Added extra extensions to viewmodel

--- infrastructure/core-view/src/main/kotlin/eu/antoniolopez/playground/core/view/extension/ViewModelExtensions.kt
+++ infrastructure/core-view/src/main/kotlin/eu/antoniolopez/playground/core/view/extension/ViewModelExtensions.kt
@@ -7,6 +7,25 @@ fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) ->
     liveData.observe(this, Observer(body))
 }
 
+inline fun <reified T : ViewModel> Fragment.getViewModel(): T {
+    return ViewModelProviders.of(this)[T::class.java]
+}
+
+inline fun <reified T : ViewModel> Fragment.withViewModel(body: T.() -> Unit): T {
+    val vm = getViewModel<T>()
+    vm.body()
+    return vm
+}
+
+inline fun <reified T : ViewModel> Fragment.withViewModel(
+    crossinline factory: () -> T,
+    body: T.() -> Unit
+): T {
+    val vm = getViewModel(factory)
+    vm.body()
+    return vm
+}
+
 inline fun <reified T : ViewModel> Fragment.getViewModel(crossinline factory: () -> T): T {
     val vmFactory = object : ViewModelProvider.Factory {
         override fun <U : ViewModel> create(modelClass: Class<U>): U = factory() as U
